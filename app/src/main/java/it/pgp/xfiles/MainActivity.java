a7dc2cf8c04118fa20dafbe9de8548e44a12ac97
pgp <pgp@gmx.it>
2020-05-07 12:41:05 +0200

    refactor duplicated variable

--- app/src/main/java/it/pgp/xfiles/MainActivity.java
+++ app/src/main/java/it/pgp/xfiles/MainActivity.java
@@ -785,13 +785,7 @@ public class MainActivity extends EffectActivity {
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            getWindow().getDecorView().setSystemUiVisibility(fullScreenVisibility);
         }
         /**
          * Web source:
