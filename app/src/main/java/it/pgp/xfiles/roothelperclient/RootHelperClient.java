pgp/XFiles

commit f8562a87aa4595b48c87e8608c10d0fa464444e0

add progress for create file operation in advanced options mode - for now only logged, not bound to a progress bar yet (reference RH version: 1.9.3_20221017)

app/build.gradle
@@ -8,7 +8,7 @@ android {
         minSdkVersion 19
         targetSdkVersion 30
         multiDexEnabled true
-        versionCode 193221011
+        versionCode 193221017
         versionName "1.9.3"
         externalNativeBuild {
             cmake {

app/src/main/java/it/pgp/xfiles/roothelperclient/RootHelperClient.java
@@ -872,16 +872,26 @@ public class RootHelperClient implements FileOperationHelper {
 
     @Override
     public void createFileOrDirectory(BasePathContent path, FileMode fileOrDirectory, FileCreationAdvancedOptions... fileOptions) throws IOException {
-        SinglePath_rq req = (fileOrDirectory == FileMode.FILE && fileOptions.length>0) ?
+        SinglePath_rq req = (fileOrDirectory == FileMode.FILE && fileOptions.length > 0) ?
                 new create_rq(path.dir, fileOptions[0]):
                 new create_rq(path.dir, fileOrDirectory);
         int errno;
+        String errMsg;
         StreamsPair rs = getStreams(path,true);
         req.write(rs.o);
         Log.d("roothelperclient","Create request sent");
         errno = Misc.receiveBaseResponse(rs.i);
-        String errMsg;
-        if(errno == 0) return;
+        if(errno == 0 && fileOptions.length > 0) {
+            // receive progress
+            long progress;
+            do {
+                progress = Misc.receiveTotalOrProgress(rs.i);
+                Log.d("rhc", "progress: "+progress);
+            }
+            while(progress != EOF_ind);
+            Log.d("rhc", "end of progress received");
+            return;
+        }
         else if(errno == 17)
             errMsg = ALREADY_EXIST;
         else

commit 2ee42267fa374b552dcf76d6e4fa887c7665981d

fix bug introduced in commit f8562a87, which made sftp file transfer fail by creating an empty local file instead

app/build.gradle
@@ -8,7 +8,7 @@ android {
         minSdkVersion 19
         targetSdkVersion 33
         multiDexEnabled true
-        versionCode 196230619
+        versionCode 196230712
         versionName "1.9.6"
         externalNativeBuild {
             cmake {

app/src/main/java/it/pgp/xfiles/roothelperclient/RootHelperClient.java
@@ -881,20 +881,22 @@ public class RootHelperClient implements FileOperationHelper {
         req.write(rs.o);
         Log.d("roothelperclient","Create request sent");
         errno = Misc.receiveBaseResponse(rs.i);
-        if(errno == 0 && fileOptions.length > 0) {
-            // receive progress
-            FileCreationAdvancedOptions fopts = fileOptions[0];
-            long progress;
-            long total = fopts.size;
-            do {
-                progress = Misc.receiveTotalOrProgress(rs.i);
-                task.publishProgressWrapper(new Pair<>(progress, total));
-            }
-            while(progress != EOF_ind);
+        if(errno == 0) {
+            if(fileOptions.length > 0) { // if a non-empty regular file is being created
+                // receive progress
+                FileCreationAdvancedOptions fopts = fileOptions[0];
+                long progress;
+                long total = fopts.size;
+                do {
+                    progress = Misc.receiveTotalOrProgress(rs.i);
+                    task.publishProgressWrapper(new Pair<>(progress, total));
+                }
+                while(progress != EOF_ind);
 
-            if(fopts.strategy.mode == FileCreationAdvancedOptions.FileCreationMode.RANDOM &&
-                    fopts.strategy.outputHashType != null) {
-                return Misc.receiveStringWithLen(rs.i); // output hash, to be shown in dialog
+                if(fopts.strategy.mode == FileCreationAdvancedOptions.FileCreationMode.RANDOM &&
+                        fopts.strategy.outputHashType != null) {
+                    return Misc.receiveStringWithLen(rs.i); // output hash, to be shown in dialog
+                }
             }
             return null;
         }